0acb04ab86680819c20b1a3114ea45a62573ee5f
Damir Yakupov <yakupovdamir93@gmail.com>
2022-11-11 09:33:50 -0500

    add removefirstlevel

--- removeFirstLevel/removeFirstLevel.js
+++ removeFirstLevel/removeFirstLevel.js
@@ -1,3 +1,9 @@
 // BEGIN (write your solution here)
-
+const makeFlat = function (arr) {
+    return arr.reduce(function (a, c) {
+      let v = c instanceof Array ? makeFlat(c) : c;
+      return a.concat(v)
+    }, [])
+  }
+  export default makeFlat;
 // END
