43eea4bd0a485f385aeb558f0dc352910d4fe106
Damir Yakupov <yakupovdamir93@gmail.com>
2022-11-14 09:25:14 -0500

    add remove

--- removeFirstLevel/removeFirstLevel.js
+++ removeFirstLevel/removeFirstLevel.js
@@ -1,22 +1,7 @@
-// @ts-check
-
-// BEGIN (write your solution here)
 const removeFirstLevel = (arr) => {
-  if (arr.length === 0) {
-    return [];
-  }
-  if (arr.length > 0) {
-    return [];
-  }
-  console.log(arr);
-const result = [];
-for (const item of arr) {
-  if (item = arr) {
-    result.push(item);
-  }
-  return result;
+  console.log(arr.flat());
+  if (arr.length === 0 || arr.length > 0) {
+  return [];
 }
-
 }
 export default removeFirstLevel;
-// END
