96f1591df1ac1d3637775d4f877558c3c18b5096
Damir Yakupov <yakupovdamir93@gmail.com>
2022-11-15 23:39:04 -0500

    add generator

--- generator/generator.js
+++ generator/generator.js
@@ -17,15 +17,29 @@
 
 
 
-// @ts-check
+// @ts-nocheck
 
-import { mkdir, mkfile } from '@hexlet/immutable-fs-trees';
+import { mkdir, mkfile } from 'immutable-fs-trees';
 
 // BEGIN (write your solution here)
-const generator = mkdir('etc', [
-  mkfile('bashrc'),
-  mkdir('consul', [
-    mkfile('config.json'),
-  ]),
-]);
+export default function tree () {
+  const generate = mkdir('nodejs-package', [
+    mkfile('Makefile'),
+    mkfile('README.md'),
+    mkdir('dist'),
+    mkdir('__tests__', [ 
+      mkfile('half.test.js', {type: 'text/javascript'})
+    ]),
+    mkfile('babel.config.js', { type: 'text/javascript' }),
+    mkdir('node_modules', [
+      mkdir('@babel', [
+        mkdir('cli', [
+          mkfile('LICENSE')
+        ])
+      ])
+    ], { owner: 'root', hidden: false })
+  ], { hidden: true })
+  console.log(generate);
+  return generate;
+}
 // END
