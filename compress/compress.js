995c915bf51c1a33ac0955f1948d0b7c39cfa060
Damir Yakupov <yakupovdamir93@gmail.com>
2022-11-18 09:29:24 -0500

    add compress

--- compress/compress.js
+++ compress/compress.js
@@ -1,6 +1,10 @@
 // BEGIN (write your solution here)
 export function compressImages (dir) {
     console.log(dir);
+    const [file] = getChildren(dir);
     
+    getName(file);
+    getMeta(file);
+    console.log(getMeta(file));
   }
   // END
