2e72514eed16ee328f608c654ed7ea59e2aa8398
Damir Yakupov <yakupovdamir93@gmail.com>
2022-11-21 23:33:05 -0500

    add compress

--- compress/compress.js
+++ compress/compress.js
@@ -1,16 +1,17 @@
 // BEGIN (write your solution here)
 export function compressImages(tree) {
+
   const children = getChildren(tree);
   const newChildren = children.map((child) => {
     const name = getName(child);
     const newMeta = _.cloneDeep(getMeta(child));
     if (isFile(child)) {
-      return mkdir(name.toLowerCase(), getChildren(child), newMeta);
+      return mkdir(name, getChildren(child), newMeta.size / 2);
     }
-    return mkfile(name.toLowerCase(), newMeta);
+    return mkfile(name, newMeta.size / 2);
   });
   const newMeta = _.cloneDeep(getMeta(tree));
-  const tree2 = mkdir(getName(tree), newChildren, newMeta);
+  const tree2 = mkdir(getName(tree), newChildren, newMeta.size / 2);
   console.log(tree2);
 }
   // END
