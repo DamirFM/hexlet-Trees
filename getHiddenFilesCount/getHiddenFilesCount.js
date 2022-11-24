8bedbc85adbfd661fd73767e901922b536dbfd79
Damir Yakupov <yakupovdamir93@gmail.com>
2022-11-24 09:58:01 -0500

    add getHiddenFilesCount

---
+++ getHiddenFilesCount/getHiddenFilesCount.js
@@ -0,0 +1,14 @@
+export default function getHiddenFilesCount (tree) {
+    if (isFile(tree)) {
+      return 1;
+    }
+    const children = getChildren(tree);
+    // Самая сложная часть
+    // Считаем количество потомков для каждого из детей,
+    // вызывая рекурсивно нашу функцию getNodesCount
+    const descendantCounts = children.map(getHiddenFilesCount);
+    // Возвращаем 1 (текущая директория) + общее количество потомков
+    console.log(descendantCounts)
+    return 1 + _.sum(descendantCounts);
+    
+  }
