802030d0747c0037e4f654fcddba0cb5e2b92dea
Damir Yakupov <yakupovdamir93@gmail.com>
2022-12-13 23:43:12 -0500

    add Cange

--- CangeClass/ChangeClass.js
+++ CangeClass/ChangeClass.js
@@ -2,5 +2,21 @@
 import _ from 'lodash';
 
 // BEGIN (write your solution here)
-
+export default function changeClass (tree ) {
+    console.log(tree)
+    const className = _.cloneDeep(className(tree))
+      const filtered = tree.children
+        .map((node) => {
+          // Перед фильтрацией отфильтровываем всех потомков
+          if (node.type === 'tag-internal') {
+    
+            // Тут самый важный момент. Рекурсивно вызываем функцию фильтрации.
+            // Дальнейшая работа не завершится, пока функция фильтрации не отфильтрует вложенные пустые узлы.
+    
+            return changeClass(node);
+          }
+          return node;
+        })
+       
+    }
 // END
