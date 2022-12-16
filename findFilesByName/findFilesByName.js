65daac997688b989022c852f704bc9c4d0048d43
Damir Yakupov <yakupovdamir93@gmail.com>
2022-12-15 23:47:36 -0500

    add

--- findFilesByName/findFilesByName.js
+++ findFilesByName/findFilesByName.js
@@ -9,10 +9,9 @@ const findFilesByName = (tree, substr) => {
       const children = getChildren(node);
       return children.flatMap((child) => iter(child, newAncestry));
     };
-  
     return iter(tree, '');
+    
   };
-  
   export default findFilesByName;
   // END
   
